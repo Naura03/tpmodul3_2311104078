6d88b16df24d8755b6e46121ea55fee01fee5143
Naura03 <nauraaisha123@gmail.com>
2025-03-03 23:21:34 +0700

    menambahkan implementasi dengan state-based construction

---
+++ tpmodul3_2311104078/DoorMachine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class DoorMachine
+{
+    public enum State { Terkunci, Terbuka }
+
+    private State currentState;
+
+    public DoorMachine()
+    {
+        currentState = State.Terkunci;
+    }
+
+    public void BukaPintu()
+    {
+        if (currentState == State.Terkunci)
+        {
+            currentState = State.Terbuka;
+            Console.WriteLine("Pintu terbuka");
+        }
+    }
+
+    public void KunciPintu()
+    {
+        if (currentState == State.Terbuka)
+        {
+            currentState = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+    }
+}
--- tpmodul3_2311104078/Program.cs
+++ tpmodul3_2311104078/Program.cs
@@ -4,15 +4,8 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Masukkan nama kelurahan:");
-        string? kelurahan = Console.ReadLine(); // Pakai nullable string (string?)
-
-        // Jika kelurahan null atau kosong, berikan nilai default "Tidak diketahui"
-        kelurahan = kelurahan ?? "Tidak diketahui";
-
-        // Memanggil method getKodePos() dari class KodePos
-        string kodePos = KodePos.getKodePos(kelurahan);
-
-        Console.WriteLine($"Kode Pos {kelurahan}: {kodePos}");
+        DoorMachine pintu = new DoorMachine();
+        pintu.BukaPintu();
+        pintu.KunciPintu();
     }
 }
